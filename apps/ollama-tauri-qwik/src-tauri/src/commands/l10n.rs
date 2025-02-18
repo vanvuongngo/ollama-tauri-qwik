0f18ef612ffcf72cc712d92c081a7a01390d995e
Van Vuong Ngo <20791239+vanvuongngo@users.noreply.github.com>
2025-02-18 10:28:34 +0100

    feat: connect to ollama and show the answer in Qwik FE

--- apps/ollama-tauri-qwik/src-tauri/src/commands/l10n.rs
+++ apps/ollama-tauri-qwik/src-tauri/src/commands/l10n.rs
@@ -1,26 +1,60 @@
 use handlebars::Handlebars;
+use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
+use std::collections::HashMap;
+use tauri::{AppHandle, Emitter};
 
-const TRANSLATE: &str = "translate to {{language}}: ";
+const TRANSLATE_PROMPT: &str = "translate without notes to {{language}}:";
 
-// translate
+#[derive(Serialize, Deserialize, Debug)]
+struct Json {
+    response: Option<String>,
+}
+
+#[derive(Clone, Serialize)]
+struct Payload {
+    message: String,
+}
+
 #[tauri::command]
-pub async fn t(language: String, prompt_message: String) -> Result<String, String> {
-  if prompt_message.is_empty() {
-    return Err("Please provide a prompt message".to_string())
-  }
+pub async fn t(app: AppHandle, language: String, prompt_message: String) -> Result<(), String> {
+    if prompt_message.is_empty() {
+        return Err("Please provide a prompt message".to_string());
+    }
 
-  let p = format!("{}{}", TRANSLATE, prompt_message);
+    let p = format!("{}{}", TRANSLATE_PROMPT, prompt_message);
 
-  let mut handlebars = Handlebars::new();
-  match handlebars.register_template_string("p", p) {
-    Ok(_) => println!("Template registered successfully"),
-    Err(e) => return Err(format!("Failed to register template: {}", e)),
-  }
+    let mut handlebars = Handlebars::new();
+    match handlebars.register_template_string("p", p) {
+      Ok(_) => {
+      },
+      Err(e) => return Err(format!("Failed to register template: {}", e)),
+    }
 
-  let data = BTreeMap::from([("language", language)]);
-  match handlebars.render("p", &data) {
-    Ok(rendered) => Ok(rendered),
-    Err(e) => Err(format!("Failed to render template: {}", e)),
-  }
+    let data = BTreeMap::from([("language", language)]);
+
+    match handlebars.render("p", &data) {
+      Ok(rendered) => {
+        let client = reqwest::Client::new();
+        let mut map = HashMap::new();
+        map.insert("model", "llama3.2".to_string());
+        map.insert("prompt", rendered);
+
+        let mut res = client.post("http://127.0.0.1:11434/api/generate")
+            .json(&map)
+            .send()
+            .await
+            .unwrap();
+
+        while let Some(chunk) = res.chunk().await.expect("Failed on chunks") {
+          let json: Json = serde_json::from_slice(&chunk).expect("Failed on parsing");
+          let message = json.response.unwrap_or("".into());
+
+          app.emit("t-answer", Payload { message: message }).unwrap();
+        }
+
+        Ok(())
+      }
+      Err(e) => Err(format!("Failed to render template: {}", e))
+    }
 }
--- apps/ollama-tauri-qwik/src/routes/index.tsx
+++ apps/ollama-tauri-qwik/src/routes/index.tsx
@@ -1,23 +1,37 @@
-import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { invoke } from "@tauri-apps/api/core";
+import { listen } from "@tauri-apps/api/event";
+
+type TranslateAnswer = {
+  message: string;
+};
 
 export default component$(() => {
+  const message = useSignal("");
+
   // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(async () => {
+    listen<TranslateAnswer>("t-answer", (event) => {
+      message.value += event.payload.message;
+      console.log(`payload: ${JSON.stringify(event.payload, null, 2)}`);
+    });
+
     invoke("t", {
+      // german, vietnamese, italian, mandarin-chinese, hindi, spanish, french,
+      // Modern-Standard Arabic, Standard Arabic, Bengali, Portuguese, Russian,
+      // Urdu (india, parkistan), Japanese,Egyptian Arabic, Turkish,Yue Chinese,
+      // Swahili, Tagalog (filipino), Korean, Iranian Persian, Thai, Levantine Arabic
       language: "german",
       promptMessage:
         "Hi, I am your personal assistent. How can I help you today?",
-    })
-      .then((message) => console.log("ANSWER:", message))
-      .catch((error) => console.error(error));
+    }).catch((error) => console.error(error));
   });
 
   return (
     <>
-      <h1>Hi</h1>
-      <div>Happy coding.</div>
+      <h1>Ollama Tauri Qwik demo</h1>
+      <div>{message.value}</div>
     </>
   );
 });
