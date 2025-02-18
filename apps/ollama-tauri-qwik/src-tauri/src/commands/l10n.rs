fe3ae170f6b1315092e77588f256dfb6e4fdcd39
Van Vuong Ngo <20791239+vanvuongngo@users.noreply.github.com>
2025-02-18 15:10:28 +0100

    feat: add language select UI; chore

--- apps/ollama-tauri-qwik/src-tauri/src/commands/l10n.rs
+++ apps/ollama-tauri-qwik/src-tauri/src/commands/l10n.rs
@@ -4,7 +4,7 @@ use std::collections::BTreeMap;
 use std::collections::HashMap;
 use tauri::{AppHandle, Emitter};
 
-const TRANSLATE_PROMPT: &str = "translate without notes to {{language}}:";
+const TRANSLATE_PROMPT: &str = "translate without description and without note to {{language}}:";
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Json {
@@ -13,7 +13,23 @@ struct Json {
 
 #[derive(Clone, Serialize)]
 struct Payload {
-    message: String,
+    chunk: String,
+}
+
+fn render_text(text_to_translate: &str, target_language: &str) -> Result<String, String> {
+    let prompt = format!("{} {}", TRANSLATE_PROMPT, text_to_translate);
+
+    let mut handlebars = Handlebars::new();
+    match handlebars.register_template_string("p", prompt) {
+        Ok(_) => {
+            let data = BTreeMap::from([("language", target_language)]);
+            match handlebars.render("p", &data) {
+                Ok(rendered) => Ok(rendered),
+                Err(e) => return Err(format!("Failed to render template: {}", e)),
+            }
+        }
+        Err(e) => return Err(format!("Failed to register template: {}", e)),
+    }
 }
 
 #[tauri::command]
@@ -22,39 +38,28 @@ pub async fn t(app: AppHandle, language: String, prompt_message: String) -> Resu
         return Err("Please provide a prompt message".to_string());
     }
 
-    let p = format!("{}{}", TRANSLATE_PROMPT, prompt_message);
+    match render_text(&prompt_message, &language) {
+        Ok(rendered_text) => {
+            let client = reqwest::Client::new();
+            let mut map = HashMap::new();
+            map.insert("model", "llama3.2".to_string());
+            map.insert("prompt", rendered_text);
 
-    let mut handlebars = Handlebars::new();
-    match handlebars.register_template_string("p", p) {
-      Ok(_) => {
-      },
-      Err(e) => return Err(format!("Failed to register template: {}", e)),
-    }
+            let mut res = client
+                .post("http://127.0.0.1:11434/api/generate")
+                .json(&map)
+                .send()
+                .await
+                .unwrap();
 
-    let data = BTreeMap::from([("language", language)]);
+            while let Some(chunk) = res.chunk().await.expect("Failed on chunks") {
+                let json: Json = serde_json::from_slice(&chunk).expect("Failed on parsing");
+                let chunk = json.response.unwrap_or("".into());
+                app.emit("t-answer", Payload { chunk }).unwrap();
+            }
 
-    match handlebars.render("p", &data) {
-      Ok(rendered) => {
-        let client = reqwest::Client::new();
-        let mut map = HashMap::new();
-        map.insert("model", "llama3.2".to_string());
-        map.insert("prompt", rendered);
-
-        let mut res = client.post("http://127.0.0.1:11434/api/generate")
-            .json(&map)
-            .send()
-            .await
-            .unwrap();
-
-        while let Some(chunk) = res.chunk().await.expect("Failed on chunks") {
-          let json: Json = serde_json::from_slice(&chunk).expect("Failed on parsing");
-          let message = json.response.unwrap_or("".into());
-
-          app.emit("t-answer", Payload { message: message }).unwrap();
+            Ok(())
         }
-
-        Ok(())
-      }
-      Err(e) => Err(format!("Failed to render template: {}", e))
+        Err(e) => Err(format!("Failed to render template: {}", e)),
     }
 }
--- apps/ollama-tauri-qwik/src/routes/index.tsx
+++ apps/ollama-tauri-qwik/src/routes/index.tsx
@@ -4,34 +4,83 @@ import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 
 type TranslateAnswer = {
-  message: string;
+  chunk: string;
 };
 
+const languages = [
+  "english",
+  "german",
+  "vietnamese",
+  "italian",
+  "mandarin-chinese",
+  "hindi",
+  "spanish",
+  "french",
+  "modern-standard arabic",
+  "standard arabic",
+  "bengali",
+  "portuguese",
+  "russian",
+  "urdu",
+  "japanese",
+  "egyptian arabic",
+  "turkish",
+  "yue chinese",
+  "swahili",
+  "tagalog",
+  "Korean",
+  "iranian persian",
+  "Thai",
+  "Levantine Arabic",
+];
+const PROMPT_MESSAGE =
+  "Hi, I am your personal assistent. How can I help you today?";
+
 export default component$(() => {
-  const message = useSignal("");
+  const message = useSignal(PROMPT_MESSAGE);
 
   // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(async () => {
     listen<TranslateAnswer>("t-answer", (event) => {
-      message.value += event.payload.message;
-      console.log(`payload: ${JSON.stringify(event.payload, null, 2)}`);
+      message.value += event.payload.chunk;
     });
-
-    invoke("t", {
-      // german, vietnamese, italian, mandarin-chinese, hindi, spanish, french,
-      // Modern-Standard Arabic, Standard Arabic, Bengali, Portuguese, Russian,
-      // Urdu (india, parkistan), Japanese,Egyptian Arabic, Turkish,Yue Chinese,
-      // Swahili, Tagalog (filipino), Korean, Iranian Persian, Thai, Levantine Arabic
-      language: "german",
-      promptMessage:
-        "Hi, I am your personal assistent. How can I help you today?",
-    }).catch((error) => console.error(error));
   });
 
   return (
     <>
       <h1>Ollama Tauri Qwik demo</h1>
-      <div>{message.value}</div>
+
+      <fieldset>
+        <div>
+          <label for="language-select">Choose a language:</label>
+          <select
+            name="language"
+            id="language-select"
+            onChange$={(e) => {
+              if (e.target?.value) {
+                e.target.checked = true;
+                if (e.target.value === "english") {
+                  message.value = PROMPT_MESSAGE;
+                  return;
+                }
+
+                message.value = "";
+                invoke("t", {
+                  language: e.target.value,
+                  promptMessage: PROMPT_MESSAGE,
+                }).catch((error) => console.error(error));
+              }
+            }}
+          >
+            {languages.map((language) => (
+              <option key={language} value={language}>
+                {language}
+              </option>
+            ))}
+          </select>
+        </div>
+      </fieldset>
+      <div class="message">{message.value}</div>
     </>
   );
 });
